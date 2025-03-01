0da8750278b2e736f7e7de4a0729bb4a8530abdb
elreydetoda <10230166+elreydetoda@users.noreply.github.com>
2025-03-01 13:20:30 -0500

    adding devenv

---
+++ devenv.nix
@@ -0,0 +1,97 @@
+{ pkgs, lib, config, inputs, ... }:                                                                                                                                                                                                                                                                                                                                                           
+                                                                                                                                                                                                                                                                                                                                                                                              
+{                                                                                                                                                                                                                                                                                                                                                                                             
+  # https://devenv.sh/basics/                                                                                                                                                                                                                                                                                                                                                                 
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [
+    git
+    curl
+    awscli2
+    jq
+    nodePackages.aws-cdk
+  ];
+
+  # https://devenv.sh/languages/
+  # languages.rust.enable = true;
+  languages = {
+    python = {
+      enable = true;
+      uv = {
+        enable = true;
+      };
+    };
+  };
+
+  # https://devenv.sh/processes/
+  # processes.cargo-watch.exec = "cargo-watch";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts = {
+    # ${config.languages.python.uv.package}/bin/uv run "$@"
+    # aws_profile_prep = {
+    #   exec = ''
+    #     set -euxo pipefail
+    #     mkdir -p ~/.aws
+    #     for profilez in infra dev-infra ; do
+    #       echo "[profile \"$profilez\"]"
+    #       echo "region = $AWS_REGION"
+    #       echo "output = json"
+    #     done >  ~/.aws/config
+    #   '';
+    # };
+    # aws_profile_prod_prep = {
+    #   exec = ''
+    #     set -euo pipefail
+    #     aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID --profile infra
+    #     aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY --profile infra
+    #     aws configure set aws_session_token $AWS_SESSION_TOKEN --profile infra
+    #     ## for debugging
+    #     # aws --profile infra sts get-caller-identity
+    #   '';
+    # };
+    # aws_profile_dev_prep = {
+    #   exec = ''
+    #     set -euo pipefail
+    #     aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID --profile dev-infra
+    #     aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY --profile dev-infra
+    #     aws configure set aws_session_token $AWS_SESSION_TOKEN --profile dev-infra
+    #     ## for debugging
+    #     # aws --profile dev-infra sts get-caller-identity
+    #   '';
+    # };
+    aws_sync_ll_folder = {
+      exec = ''
+        set -euxo pipefail
+        aws s3 sync $ARTIFACTS_FOLDER s3://$BUCKET_NAME/ll/
+      '';
+    };
+  };
+
+  # enterShell = ''
+  #   git --version
+  # '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  # #   "myproj:setup".exec = "mytool build";
+  # #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # #   before = [ "devenv:enterShell" "devenv:enterTest" ];
+  #   };
+  # };
+
+  # https://devenv.sh/tests/
+  # enterTest = ''
+  #   echo "Running tests"
+  #   git --version | grep --color=auto "${pkgs.git.version}"
+  # '';
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
